793120d5339042f1ba6deaf26c0044f2fdb529f0
Joyce Ma <joycema@Joyces-MacBook-Air.local>
2024-10-06 20:32:03 -0500

    updated new shuffle functions, added more clothes, and fixed text

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 document.querySelectorAll("button")[0].addEventListener("click", shuffleAll);
 document.querySelectorAll("button")[1].addEventListener("click", shuffleTops);
 document.querySelectorAll("button")[2].addEventListener("click", shuffleBottoms);
+document.querySelectorAll("button")[3].addEventListener("click", shuffleJackets);
 
 function shuffleAll() {
     var button = document.querySelectorAll("button")[0];
     document.querySelectorAll("button")[1].click();
     document.querySelectorAll("button")[2].click();
+    document.querySelectorAll("button")[3].click();
 }
 
 function shuffleTops() {
@@ -17,12 +19,21 @@ function shuffleTops() {
     button.setAttribute("data-bs-slide-to", "" + randomTop + "");
 }
 
-function shuffleBottoms(button) {
+function shuffleBottoms() {
     var button = document.querySelectorAll("button")[2];
     var bottoms = document.querySelectorAll(".bottoms");
     var activeBottoms = document.querySelector("#carouselBottoms .active .bottoms");
     var numBottoms = bottoms.length;
     var randomBottoms = Math.floor(Math.random() * numBottoms);
+
+    button.setAttribute("data-bs-slide-to", "" + randomBottoms + "");
+}
+
+function shuffleJackets() {
+    var button = document.querySelectorAll("button")[3];
+    var jackets = document.querySelectorAll(".jackets");
+    var numJackets = jackets.length;
+    var randomBottoms = Math.floor(Math.random() * numJackets);
 
     button.setAttribute("data-bs-slide-to", "" + randomBottoms + "");
 }
